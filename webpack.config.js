7b248ff147a094b89a54819f7bff14536a74f54c
nikolai.b <nbardzinkevichh@gmail.com>
2024-11-18 09:18:48 +0800

    fix configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
   output: {
-    publicPath: '/',
     clean: true,
   },
   module: {
@@ -32,7 +30,7 @@ export default {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './index.html',
     }),
   ],
   
