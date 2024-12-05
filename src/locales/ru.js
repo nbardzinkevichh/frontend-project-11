7f5517332abef557cad11c31da61a490466655e9
nikolai.b <nbardzinkevichh@gmail.com>
2024-12-05 08:33:59 +0800

    ru locale i18next

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -4,8 +4,6 @@ export default {
       networkError: 'Ошибка сети',
     },
     header: {
-      // title: 'RSS агрегатор',
-      // leadParagraph: 'Начните читать RSS сегодня! Это легко, это красиво.',
       form: {
         button: 'Добавить',
         infoMessages: {
