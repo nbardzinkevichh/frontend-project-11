514661ef7b76cf8428239bb5acd80ccdac6a45c1
nikolai.b <nbardzinkevichh@gmail.com>
2024-12-05 11:34:48 +0800

    remove airbnb config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['**/dist/', '**/webpack.config.js', './eslint.config.js']
+    ignores: ['**/dist/*', '**/webpack.config.js', './eslint.config.js']
   },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
